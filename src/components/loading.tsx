1551dcd7bf8dada42015db8b4efa8e54aadbe387
Kauê Reis <kaue0607@gmail.com>
2024-02-08 05:06:38 +1300

    Update loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,11 @@
+import "./loading.css"
+
+const LoadScreen = () => {
+  return (
+    <div className="wrapper">
+      <span className="rotate"/>
+    </div>
+  );
+};
+
+export default LoadScreen;
